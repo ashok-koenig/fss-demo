2f198d8aa23371f3d29aeacc2cb4ad42485e73dd
Ashok S <ashok.s@koenig-solutions.com>
2025-03-18 15:09:06 +0530

    Products REST API

---
+++ 01-rest-api/src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Product;
+import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.stream.events.EntityReference;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(product));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id){
+        Product product = productService.getProductById(id).orElseThrow();
+        return ResponseEntity.ok(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product){
+        productService.getProductById(id).orElseThrow();
+        product.setId(id);
+        return ResponseEntity.ok(productService.updateProduct(product));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProductById(@PathVariable Long id){
+        productService.getProductById(id).orElseThrow();
+        productService.deleteProductById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
+
+}
